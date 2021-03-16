fe39fac9c6b2e49dcdf42750d8f513d285e8ce01
BaffledBanana <rolands.laucis@gmail.com>
2021-03-16 13:29:21 +0200

    oh fuck me, now the keys work

--- The_Bard.js
+++ The_Bard.js
@@ -4,7 +4,7 @@ import Discord from 'discord.js'
 import fs from 'fs'
 
 //read access keys and tokens of the bot account from file outside the root dir of this bot (and outside of the git repo folder)
-var token = JSON.parse(fs.readFileSync('../keys.json'))['token']
+var token = JSON.parse(fs.readFileSync('../keys.json'))
 
 console.log('starting up The Bard...')
 const client = new Discord.Client();
