d85fe7dc7a5cc47ca9f2043424fa367449351040
BaffledBanana <rolands.laucis@gmail.com>
2021-05-15 19:59:54 +0300

    embeds and code upgrade for future features

--- The_Bard.js
+++ The_Bard.js
@@ -14,10 +14,71 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.author.id != "821327316290502706"){
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }
+
+    var reply = ParseCommand(msg)
+
+    if (reply != "") {
+      msg.channel.send(reply)
+        .then(() => console.log(`Sent a reply to ${msg.author.username}`))
+        .catch(console.error);
+    }
   }
+
 });
+
+
+var cmdPrefix = '!'
+var commands = {
+  "cmd functions": {
+    "help": Help,
+    "embed": EmbedCmd
+  },
+  "cmd descriptions":{
+    "help": "Lists all available bot commands and their descriptions, usage.",
+    "embed": "Prints sample embed."
+  }
+}
+
+function ParseCommand(msg) {
+  var text = msg.content
+  if (text[0] == cmdPrefix) {
+    var cmd = text.split(' ')[0].replace(cmdPrefix, '') //removes the prefix
+    return commands['cmd functions'][cmd](msg)
+  } else {
+    return ""
+  }
+}
+
+function Help(){
+  var msg = "\n"
+  Object.keys(commands['cmd descriptions']).forEach(key => {
+    msg += "``" + cmdPrefix + key + "`` : " + commands['cmd descriptions'][key] + "\n"
+  });
+  return msg
+}
+
+function EmbedCmd(msg) {
+  var exampleEmbed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle(msg.author.username + '\'s stats')
+    
+    //these should be read from a .json local file or database, because we know msg.author.id and msg.author.username. Should be easy to fetch all this data for the "value" part:
+    .addFields(
+      { name: 'Influence', value: '#5' },
+      { name: 'Coins', value: '100', inline: true },
+      { name: 'Favorite emoji', value: '😂', inline: true },
+      { name: 'Others react with', value: '🤡', inline: true } //unicode ir sarežģīts iekš js, jo sastāv no vairākiem \uF269 char kā kuram emoji. Jāskatās, ko discord.js atgriež, kad bots strādā ar emoji un tas jāizmanto.
+    )
+    .setTimestamp()
+  
+  console.log("Sending out embed message...")
+  msg.channel.send(exampleEmbed)
+  return ""
+}
 
 //push in the keys variable
 client.login(token);
